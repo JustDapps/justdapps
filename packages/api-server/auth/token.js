c394aab28848be7233956900f5872b165d286a65
olekon <kondrahanov@gmail.com>
2019-11-05 16:23:32 +0700

    generateToken changed

--- packages/api-server/auth/token.js
+++ packages/api-server/auth/token.js
@@ -7,13 +7,12 @@ const createToken = (auth) => jwt.sign({
   expiresIn: 60 * 120,
 });
 
+const generateToken = (req, res, next) => {
+  req.token = createToken(req.auth);
+  res.setHeader('x-auth-token', req.token);
+  return res.status(200).send(JSON.stringify(req.user));
+};
+
 module.exports = {
-  generateToken(req, res, next) {
-    req.token = createToken(req.auth);
-    return next();
-  },
-  sendToken(req, res) {
-    res.setHeader('x-auth-token', req.token);
-    return res.status(200).send(JSON.stringify(req.user));
-  },
+  generateToken,
 };
