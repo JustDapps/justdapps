f7516043bcccf2a81ec7f01909603a580ce1c088
olekon <kondrahanov@gmail.com>
2019-11-11 22:05:47 +0700

    unused param removed

--- packages/api-server/middlewares/auth.js
+++ packages/api-server/middlewares/auth.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.TOKEN_PRIVATE_KEY;
 
 module.exports = (req, res, next) => {
-  const { token } = req.cookies;
+  const {token} = req.cookies;
   if (!token) {
     res.status(401).send('Unauthorized: No token provided');
   } else {
-    jwt.verify(token, secret, (err, decoded) => {
+    jwt.verify(token, secret, (err) => {
       if (err) {
         res.status(401).send('Unauthorized: Invalid token');
       } else {
