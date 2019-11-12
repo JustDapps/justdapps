4caa94f63bf263d70a74e450bf83f68540e2adb5
olekon <kondrahanov@gmail.com>
2019-11-12 12:10:33 +0700

    linter warnings fixed

--- packages/data-mongodb/models/user.js
+++ packages/data-mongodb/models/user.js
@@ -6,7 +6,7 @@ const authTypes = {
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  authProvider: { type: String, required: true },
+  authProvider: {type: String, required: true},
   profile: {
     id: String,
     displayName: String,
@@ -14,32 +14,30 @@ const userSchema = mongoose.Schema({
 });
 
 
-userSchema.statics.upsertGoogleUser = function (id, displayName, cb) {
-  return this.findOne({
-    authProvider: authTypes.google,
-    'profile.id': id,
-  }, (err, user) => {
-
-    const format = (user) => {
-      return {id : user.profile.id, displayName: user.profile.displayName};
-    }
-
-    if (!user) {
-      const newUser = new this({
-        _id: new mongoose.Types.ObjectId(),
-        authProvider: authTypes.google,
-        profile: {
-          id,
-          displayName,
-        },
-      });
-
-      newUser.save((error, savedUser) => cb(error, format(savedUser)));
-    } else {
-      return cb(err, format(user));
-    }
+userSchema.statics.upsertGoogleUser = (id, displayName, cb) => this.findOne({
+  authProvider: authTypes.google,
+  'profile.id': id,
+}, (err, user) => {
+  const format = (userToFormat) => ({
+    id: userToFormat.profile.id,
+    displayName: userToFormat.profile.displayName,
   });
-};
+
+  if (!user) {
+    const newUser = new this({
+      _id: new mongoose.Types.ObjectId(),
+      authProvider: authTypes.google,
+      profile: {
+        id,
+        displayName,
+      },
+    });
+
+    return newUser.save((error, savedUser) => cb(error, format(savedUser)));
+  }
+  return cb(err, format(user));
+});
+
 
 const userModel = mongoose.model('User', userSchema);
 
