d1e8be0b2763fc141479bf316d81d1da54172bbf
olekon <kondrahanov@gmail.com>
2019-11-14 21:12:20 +0700

    key field added to upsert response

--- packages/data-mongodb/models/user.js
+++ packages/data-mongodb/models/user.js
@@ -14,29 +14,32 @@ const userSchema = mongoose.Schema({
 });
 
 
-userSchema.statics.upsertGoogleUser = (id, displayName, cb) => this.findOne({
-  authProvider: authTypes.google,
-  'profile.id': id,
-}, (err, user) => {
-  const format = (userToFormat) => ({
-    id: userToFormat.profile.id,
-    displayName: userToFormat.profile.displayName,
-  });
-
-  if (!user) {
-    const newUser = new this({
-      _id: new mongoose.Types.ObjectId(),
-      authProvider: authTypes.google,
-      profile: {
-        id,
-        displayName,
-      },
+userSchema.statics.upsertGoogleUser = function upsertGoogleUser(id, displayName, cb) {
+  return this.findOne({
+    authProvider: authTypes.google,
+    'profile.id': id,
+  }, (err, user) => {
+    const format = (userToFormat) => ({
+      key: userToFormat._id.toString(),
+      id: userToFormat.profile.id,
+      displayName: userToFormat.profile.displayName,
     });
 
-    return newUser.save((error, savedUser) => cb(error, format(savedUser)));
-  }
-  return cb(err, format(user));
-});
+    if (!user) {
+      const newUser = new this({
+        _id: new mongoose.Types.ObjectId(),
+        authProvider: authTypes.google,
+        profile: {
+          id,
+          displayName,
+        },
+      });
+
+      return newUser.save((error, savedUser) => cb(error, format(savedUser)));
+    }
+    return cb(err, format(user));
+  });
+};
 
 
 const userModel = mongoose.model('User', userSchema);
