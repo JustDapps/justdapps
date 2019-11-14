8c2e5004a4b58e9b6c3c7165e0697ae89800344a
olekon <kondrahanov@gmail.com>
2019-11-14 21:11:56 +0700

    saving samples implemented

--- packages/data-mongodb/examples/saveSamples.js
+++ packages/data-mongodb/examples/saveSamples.js
@@ -8,9 +8,9 @@ const clearDatabase = () => User.deleteMany({}).then(
   () => Model.deleteMany({}),
 );
 
-const populateUsers = () => User.insertMany(users);
+const saveUsers = () => User.insertMany(users);
 
-const populateModels = () => Promise.all(
+const saveModels = () => Promise.all(
   models.map((item) => {
     const newModel = new Model(item);
     return User.findOne({'profile.displayName': item.userId}).exec().then(
@@ -23,14 +23,31 @@ const populateModels = () => Promise.all(
   }),
 );
 
-mongoose.connect(process.env.DB_PATH, {useNewUrlParser: true, useUnifiedTopology: true}).then(
-  () => clearDatabase()
-    .then(populateUsers)
-    .then(populateModels)
-  // populateModels()
-    .then(() => {
-      console.log('DONE');
-      return mongoose.disconnect();
-    }),
+
+const cleanupAndSave = () => clearDatabase()
+  .then(saveUsers)
+  .then(saveModels);
+
+const selectAndPopulate = () => Model
+  .find({})
+  .populate('userId')
+  .exec()
+  .then((results) => console.log(results[0].userId.profile.displayName));
+
+const testMethods = () => Model
+  .findByUser('5dcc22f444ad273dc0852cd6', (err, results) => {
+    console.log(results);
+  });
+
+
+// connect, execute specified handler and disconnect
+const main = (handler) => mongoose.connect(
+  process.env.DB_PATH,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+).then(
+  () => handler().then(() => { console.log('DONE'); return mongoose.disconnect(); }),
   (err) => console.log(err),
 );
+
+// entry point
+main(cleanupAndSave);
