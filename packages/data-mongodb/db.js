d726b4d6e03a36b37dcaf771a263cda4d759d76d
olekon <kondrahanov@gmail.com>
2019-11-15 17:56:47 +0700

    user and model object added to export

--- packages/data-mongodb/db.js
+++ packages/data-mongodb/db.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const {User, Model} = require('./models');
 
-module.exports.connect = (path, cb) => mongoose.connect(
-  path,
-  {useNewUrlParser: true, useUnifiedTopology: true},
-  cb,
-);
+module.exports = {
+  connect: (path, cb) => mongoose.connect(
+    path,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    cb,
+  ),
 
-module.exports.upsertGoogleUser = (id, displayName, cb) => User
-  .upsertGoogleUser(id, displayName, cb);
+  user: {
+    upsertGoogleUser: (id, displayName, cb) => User
+      .upsertGoogleUser(id, displayName, cb),
+  },
+
+  model: {
+    findByUser: (userId) => Model.findByUser(userId),
+    addForUser: (modelProperties, userId) => Model.addForUser(modelProperties, userId),
+  },
+};
