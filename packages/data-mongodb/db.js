49dca0208348069e0d9a7065c7f74de392046819
olekon <kondrahanov@gmail.com>
2019-11-15 17:45:37 +0700

    formatting fixed

--- packages/data-mongodb/db.js
+++ packages/data-mongodb/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const {User, Model} = require('./models');
 
-module.exports.connect = (path, cb) => mongoose.connect(path, {useNewUrlParser: true, useUnifiedTopology: true}, cb);
+module.exports.connect = (path, cb) => mongoose.connect(
+  path,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+  cb,
+);
 
-module.exports.upsertGoogleUser = (id, displayName, cb) => User.upsertGoogleUser(id, displayName, cb);
+module.exports.upsertGoogleUser = (id, displayName, cb) => User
+  .upsertGoogleUser(id, displayName, cb);
