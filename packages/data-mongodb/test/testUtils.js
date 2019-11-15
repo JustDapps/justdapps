9feb3ac112cb435eb4ed881572ab03842bd4d7c7
olekon <kondrahanov@gmail.com>
2019-11-15 23:38:17 +0700

    addGoogleUsers rewritten

--- packages/data-mongodb/test/testUtils.js
+++ packages/data-mongodb/test/testUtils.js
@@ -11,11 +11,18 @@ module.exports.getTotalUsers = () => User
   .exec()
   .then((results) => results.length);
 
+/** Inserts array of user names to User collection, returns object
+ * {name1: id1, name2: id2, ...}
+ */
 module.exports.addGoogleUsers = (names) => Promise.all(
   names.map((name) => User
     .upsertGoogleUser(`id_${name}`, name)
-    .then((user) => user.id)),
-);
+    .then((user) => ({id: user.id, name}))),
+)
+  .then((users) => users.reduce(
+    (accum, value) => ({...accum, [value.name]: value.id}),
+    {},
+  ));
 
 module.exports.addModelsForUser = async (models, userName) => {
   const userId = await User.getUserId(userName);
