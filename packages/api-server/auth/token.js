577c1e7213a248d8618b6705a7b54bc944fd09e3
olekon <kondrahanov@gmail.com>
2019-11-18 17:31:37 +0700

    add description

--- packages/api-server/auth/token.js
+++ packages/api-server/auth/token.js
@@ -1,8 +1,16 @@
+/**
+ * This small module serves for sending authentication information back to client (browser)
+ * Exported `generateToken` middleware writes into `cookies.token` an encrypted token that contains
+ * `userId` - app user unique identifier.
+ *
+ * It requires request object to contain an auth object with the `id` property
+ */
+
 const jwt = require('jsonwebtoken');
 const config = require('../config/config.js');
 
 const createToken = (auth) => jwt.sign({
-  id: auth.id,
+  userId: auth.id,
 },
 process.env.TOKEN_PRIVATE_KEY, {
   expiresIn: config.tokenExpiresIn,
