b7e486e47a99b5f93a288364e966ff5b1ba16efe
olekon <kondrahanov@gmail.com>
2019-11-20 23:43:17 +0700

    tests initial

--- packages/api-server/test/auth.test.js
+++ packages/api-server/test/auth.test.js
@@ -1,7 +1,49 @@
+require('dotenv').config();
+// eslint-disable-next-line import/no-extraneous-dependencies
 const chai = require('chai');
+
+const passport = require('passport');
+const MockStrategy = require('passport-mock-strategy');
+const initPassport = require('../auth/passport');
+
 chai.use(require('chai-http'));
-const app = require('../app');
+const initApp = require('../app');
+
+const {expect} = chai;
+
+const mockedGoogleId = '0123456778abc';
+const mockUpsertGoogleUser = (id, displayName) => Promise.resolve({id: mockedGoogleId, displayName});
+
+// mock database
+const mockDataProvider = {
+  user: {
+    upsertGoogleUser: mockDataProvider,
+  },
+};
 
 describe('/auth', () => {
+  const app = initApp(mockDataProvider);
 
+  const userProfile = {
+    id: 'google-0123456789',
+    name: 'John Smith',
+    emails: [{value: 'jsmith@gmail.com'}],
+  };
+
+  before((hookDone) => {
+    // mock google strategy
+    passport.use(new MockStrategy({
+      name: 'google-token',
+      user: userProfile,
+    }, (user, done) => initPassport.googleAuthCallback(mockDataProvider, user, done)));
+  });
+
+
+  it('should save valid google token bearer, with displayName=e-mail and authProvider=Google', () => {
+    expect(1).equal(1);
+  });
+
+  it('in case of invalid token should return 401 error', () => {
+
+  });
 });
---
+++ packages/api-server/test/models.test.js
@@ -0,0 +1,13 @@
+/**
+ * To simulate auth process first we need to obtain application-signed token
+ * Use auth/token.js for this purpose or make a fake authentication request to /auth/google
+ * Obtained token should be sent in cookies
+ */
+
+describe('/models', () => {
+  describe('/GET', () => {
+    it('should return list of models', () => {
+
+    });
+  });
+});
