67f4f87ef01d5fa806f932913386e27ccbe491f3
olekon <kondrahanov@gmail.com>
2019-11-22 17:58:09 +0700

    database call extracted

--- packages/api-server/auth/passport.js
+++ packages/api-server/auth/passport.js
@@ -1,22 +1,25 @@
 const passport = require('passport');
 
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GoogleStrategy = require('passport-google-token').Strategy;
 
-const init = (dataSource) => {
+/**
+ * Initializes passport.js with strategies
+ */
+const init = () => {
+  /**
+   * Google strategy requires acceess_token in request body.
+   * Returns {googleid: Google Profile id, displayName: email} as user
+   */
   passport.use(
     new GoogleStrategy({
       clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
       clientSecret: process.env.GOOGLE_AUTH_SECRET,
     },
     (accessToken, refreshToken, profile, done) => {
-      dataSource.user.upsertGoogleUser(profile.id, profile.emails[0].value)
-        .then((user) => {
-          done(null, user);
-        })
-        .catch((err) => {
-          done(err, null);
-        });
+      done(null, {
+        googleId: profile.id,
+        displayName: profile.emails[0].value,
+      });
     }),
   );
 };
