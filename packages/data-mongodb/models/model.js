7af6541633762c6eb9dab01dd139bf4d43327ec2
olekon <kondrahanov@gmail.com>
2019-12-04 18:25:06 +0700

    return false in case of failure

--- packages/data-mongodb/models/model.js
+++ packages/data-mongodb/models/model.js
@@ -4,38 +4,38 @@
 
 const mongoose = require('mongoose');
 
-const {ObjectId} = mongoose.Types;
-const {modelName} = require('./user');
+const { ObjectId } = mongoose.Types;
+const { modelName } = require('./user');
 
 const entityType = mongoose.Schema({
-  name: {type: String, required: true},
-  address: {type: String, default: ''},
+  name: { type: String, required: true },
+  address: { type: String, default: '' },
   abi: String,
   code: String,
   compiler: String,
-}, {_id: false});
+}, { _id: false });
 
 const relationType = mongoose.Schema({
-  source: {type: String, required: true},
-  target: {type: String, required: true},
-  setOps: {type: String, required: true},
-  validateOps: {type: String},
-}, {_id: false});
+  source: { type: String, required: true },
+  target: { type: String, required: true },
+  setOps: { type: String, required: true },
+  validateOps: { type: String },
+}, { _id: false });
 
 const modelSchema = new mongoose.Schema({
-  _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-  userId: {type: mongoose.Schema.Types.ObjectId, ref: modelName},
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: modelName },
 
-  name: {type: String, required: true, default: ''},
-  description: {type: String, default: ''},
+  name: { type: String, required: true, default: '' },
+  description: { type: String, default: '' },
 
-  entities: {type: [entityType], default: []},
-  relations: {type: [relationType], default: []},
+  entities: { type: [entityType], default: [] },
+  relations: { type: [relationType], default: [] },
 
   dapps: {
     type: [{
       networkId: Number,
-      description: {type: String, default: ''},
+      description: { type: String, default: '' },
 
       entities: [entityType],
       relations: [relationType],
@@ -55,10 +55,10 @@ modelSchema.statics.findByUser = function findByUser(userId) {
 
 /** Checks if specified user is owner of the specified model */
 modelSchema.statics.checkOwner = function checkOwner(userId, modelId) {
-  return this.findOne({_id: new ObjectId(modelId)})
+  return this.findOne({ _id: new ObjectId(modelId) })
     .select('userId')
     .exec()
-    .then(({userId: modelUserId}) => userId === modelUserId.toString());
+    .then(({ userId: modelUserId }) => userId === modelUserId.toString());
 };
 
 /** Saves new model with specified userId and returns its _id */
@@ -77,13 +77,15 @@ modelSchema.statics.delete = function deleteById(id) {
   return this.findByIdAndDelete(id).exec().then(() => true);
 };
 
+/** Changes model with specified identifier.
+ * Properties to be changed are specified as 1st parameter */
 modelSchema.statics.update = function update(modelProperties, id) {
   return this.findByIdAndUpdate(
     id,
     modelProperties,
-    {useFindAndModify: false},
+    { useFindAndModify: false },
   ).exec()
-    .then(() => true);
+    .then((response) => (!!response));
 };
 
 
