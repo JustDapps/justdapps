354b0ef68c62903c790496b425fb9945025d0a35
olekon <kondrahanov@gmail.com>
2019-12-04 14:09:16 +0700

    cancel logging in tests

--- packages/api-server/app.js
+++ packages/api-server/app.js
@@ -22,9 +22,11 @@ module.exports = (dataSource) => {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
 
-  app.use(logger('dev'));
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+  }
   app.use(express.json());
-  app.use(express.urlencoded({extended: false}));
+  app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(dataProvider(dataSource));
 
