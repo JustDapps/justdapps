ab17b98d911e0609a43ef2b9c7a1dd00d752b1df
olekon <kondrahanov@gmail.com>
2019-12-08 18:29:15 +0700

    call implemented

--- packages/eth-core/src/eth.js
+++ packages/eth-core/src/eth.js
@@ -17,8 +17,37 @@ const Eth = function Eth(nodeProvider) {
 
 Eth.NodeProvider = NodeProvider;
 
-Eth.prototype.call = function call() {
-  throw ('NOT IMPLEMENTED YET');
+/**
+ * Calls contract method (i.e. without sending a transaction) and returns the resulted value
+ *
+ * @param {string} address address of a contract, checksum is not required
+ * @param {Object[]} abi ABI-array of a contract
+ * @param {(string|number)} networkId network id of a contract
+ * @param {string} methid name of a method to call
+ * @param {*[]} args array of arguments for a method
+ * @param {Object} options Web3 options for a call (gasPrice, from, etc.)
+ *
+ * @returns {*} single or multiple resulting values of a call
+ * @throws Exception is thrown if contract reverts or no method exists, or any other error occurs
+ */
+Eth.prototype.call = function call(address, abi, networkId, method, args = [], options = {}) {
+  const contract = this.createContract(address, abi, networkId);
+  return contract.methods[method](...args).call(options);
+};
+
+
+/**
+ * Creates contract object to call its methods later
+ *
+ *  @param {string} address address of a contract, checksum is not required
+ * @param {Object[]} abi ABI-array of a contract
+ * @param {(string|number)} networkId network id of a contract
+ *
+ * @returns {*} Web3 contract object
+ */
+Eth.prototype.createContract = function createContract(address, abi, networkId) {
+  const web3 = this.nodeProvider.web3For(networkId);
+  return new web3.eth.Contract(abi, address);
 };
 
 
