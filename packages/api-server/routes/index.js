694f71573842835fb9ab415c617c7bca78c57011
olekon <kondrahanov@gmail.com>
2019-12-16 10:25:09 +0700

    add ethRouter usage

--- packages/api-server/routes/index.js
+++ packages/api-server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authRouter = require('./auth');
 const modelsRouter = require('./models');
+const ethRouter = require('./eth');
+
 
 const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/models', modelsRouter);
-
+router.use('/eth', ethRouter);
 module.exports = router;
