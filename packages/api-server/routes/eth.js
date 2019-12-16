8e1cbafc6f64c07981291df4c36b6c439cb14c61
olekon <kondrahanov@gmail.com>
2019-12-16 17:05:31 +0700

    call inner controller

--- packages/api-server/routes/eth.js
+++ packages/api-server/routes/eth.js
@@ -1,19 +1,16 @@
 const express = require('express');
+const ethController = require('../controllers/eth.controller');
 const { requireAuth } = require('../middlewares/auth');
-const { accessModel } = require('../middlewares/access');
 
 const router = express.Router();
 
 // this route should require authentication
 router.use(requireAuth);
 
-router.post('/call',
-  accessModel,
-  (req, res, next) => {
-    req.dataSource.getEntity(req.body.modelId, req.body.dappId);
-  });
-router.post('/methodtx', null);
-router.post('/deploytx', null);
-router.post('/send', null);
+router.post('/call', ethController.callContract);
+
+// router.post('/methodtx', null);
+// router.post('/deploytx', null);
+// router.post('/send', null);
 
 module.exports = router;
